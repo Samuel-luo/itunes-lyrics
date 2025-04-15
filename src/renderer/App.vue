b2ef51408b151a12b3d7ce9832df8e433be3d1c1
Samuel Luo <samuel_lsn@foxmail.com>
2025-04-15 22:00:46 +0800

    init project

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>💖 Hello World!</h1>
+  <p>Welcome to your Electron application.</p>
+  <button>test</button>
+</template>
+
+<script setup>
+console.log('👋 This message is being logged by "renderer.ts", included via Vite');
+</script>
+
+<style lang="less" scoped>
+h1 {
+  color: red;
+}
+</style>
